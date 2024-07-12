305fee3d997b67cc2c80d5c1df0314985e76ba84
Videsh Ravi <videshravi27@gmail.com>
2024-07-12 21:07:45 +0530

    Cors

--- backend/server.js
+++ backend/server.js
@@ -13,21 +13,8 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors()); 
 
-const allowedOrigins = ['https://home-fix-pi.vercel.app/login', 'https://home-fix-0xyg.onrender.com'];
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
 
 // Log middleware
 app.use((req, res, next) => {
@@ -51,4 +38,5 @@ mongoose.connect(process.env.MONGO_URI)
     })
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error);
-    });
+    }
+);
